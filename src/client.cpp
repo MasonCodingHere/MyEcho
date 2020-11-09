a9057e61dbd36c0085c02777b12f94a9de3cc48c
xgx127 <xgx127@126.com>
2020-11-09 10:19:37 +0800

    For ALICloud Test

--- src/client.cpp
+++ src/client.cpp
@@ -6,11 +6,14 @@
  ************************************************************************/
 
 #include <iostream>
+#include <string>
 #include "../include/MyClient.h"
 
 int main(int argc, char **argv)
 {
-	MyClient myclient;
+	const std::string serverIP = argv[1];
+	const std::string serverPort = argv[2];
+	MyClient myclient(serverIP, serverPort);
 	myclient.MyGetAddrInfo();
 	myclient.GetClientFd();
 	myclient.ClientRW();
